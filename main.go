de8b31e5a2acae322b3347618b93e9053e83339f
amirrezaask <raskarpour@gmail.com>
2023-11-21 21:25:07 +0330

    create window abstraction so buffers will be rendered into windows

--- main.go
+++ main.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+var (
+	font rl.Font
+	fontSize float32
 )
 
 type Command func(Editor) error
 type Variables map[string]any
 type Key struct {
-	Ctrl bool
-	Alt bool
+	Ctrl  bool
+	Alt   bool
 	Shift bool
 	Super bool
-	K string
+	K     string
 }
 type Keymap map[Key]Command
 type Commands map[string]Command
@@ -27,11 +31,28 @@ func (p Position) String() string {
 	return fmt.Sprintf("Line: %d Column:%d\n", p.Line, p.Column)
 }
 
-type View struct {
+type Window struct {
 	BufferIndex int
-	MaxLines int
-	MaxColumns int
+	zeroLocation rl.Vector2
+	Height    int
+	Width  int
 }
+
+func (e *Editor) RenderBufferInWindow(buffer *Buffer, window *Window) {
+	for idx, line := range buffer.Content {
+		renderAt := rl.Vector2{
+			Y: float32(window.zeroLocation.Y) + measureTextSize(font, ' ', fontSize, 0).Y * float32(idx),
+			X: float32(window.zeroLocation.X),
+		}
+		rl.DrawTextEx(font, string(line), renderAt, fontSize, 0, rl.White)
+		cursorSize := measureTextSize(font, buffer.Content[buffer.Cursor.Line][buffer.Cursor.Column], fontSize, 0) //TODO: can be cached
+		cursorPosX := int32(buffer.Cursor.Column) * int32(cursorSize.X)
+		cursorPosY := int32(buffer.Cursor.Line) * int32(cursorSize.Y)
+		rl.DrawRectangleLines(cursorPosX, cursorPosY, int32(cursorSize.X), int32(cursorSize.Y), rl.Yellow)
+	}
+}
+
+
 type Buffer struct {
 	Cursor    Position
 	Content   [][]byte
@@ -42,15 +63,17 @@ type Buffer struct {
 }
 
 type Editor struct {
-	Buffers         []Buffer
-	GlobalKeymaps   []Keymap
-	GlobalVariables Variables
-	Commands        Commands
-	Views           []View
-	ActiveViewIndex int
+	Buffers           []Buffer
+	GlobalKeymaps     []Keymap
+	GlobalVariables   Variables
+	Commands          Commands
+	Windows           []Window
+	ActiveWindowIndex int
+	LineWrapping      bool
 }
+
 func (e Editor) CurrentBuffer() *Buffer {
-	return &e.Buffers[e.Views[e.ActiveViewIndex].BufferIndex]
+	return &e.Buffers[e.Windows[e.ActiveWindowIndex].BufferIndex]
 }
 
 func (buffer *Buffer) InsertCharAtCursor(char byte) error {
@@ -68,22 +91,30 @@ func main() {
 	rl.SetTargetFPS(60)
 
 	// create editor
-	editor := Editor{}
+	editor := Editor{
+		LineWrapping : true,
+	}
 
-	const textSize = 70
-	rl.SetTextLineSpacing(textSize)
+	fontSize = 70
+	rl.SetTextLineSpacing(int(fontSize))
 	rl.SetMouseCursor(rl.MouseCursorIBeam)
 	editor.Buffers = append(editor.Buffers, Buffer{
 		Cursor:   Position{0, 0},
 		Content:  [][]byte{[]byte("hello"), []byte("world")},
 		FilePath: "test.txt",
 	})
+	editor.Windows = append(editor.Windows, Window{
+		BufferIndex:   0,
+		zeroLocation:  rl.Vector2{
+			X: 0, Y: 0,
+		},
+		Height:        rl.GetRenderHeight(),
+		Width:         rl.GetRenderWidth(),
+	})
 
-	editor.Views = append(editor.Views, View{BufferIndex: 0})
-
-	font := rl.LoadFontEx("FiraCode.ttf", 100, nil)
+	font = rl.LoadFontEx("FiraCode.ttf", int32(fontSize), nil)
 	for !rl.WindowShouldClose() {
-		buffer := &editor.Buffers[editor.Views[editor.ActiveViewIndex].BufferIndex]
+		buffer := &editor.Buffers[editor.Windows[editor.ActiveWindowIndex].BufferIndex]
 
 		// execute any command that should be executed
 		cmd := defaultKeymap[MakeKey(buffer)]
@@ -96,21 +127,21 @@ func main() {
 		// Render
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Black)
-		var sb strings.Builder
-		for _, line := range buffer.Content {
-			_, _ = sb.Write(line)
-			_, _ = sb.WriteString("\n")
-		}
 
-		// render text
-		rl.DrawTextEx(font, sb.String(), rl.Vector2{X: 0, Y: 0}, textSize, 0, rl.White)
-		// render cursor
-		// we should find cursor size, for that we should measure string size of cursor
-		cursorSize := rl.MeasureTextEx(font, string(buffer.Content[buffer.Cursor.Line][buffer.Cursor.Column]), textSize, 0) //TODO: can be cached
-		cursorPosX := int32(buffer.Cursor.Column) * int32(cursorSize.X)
-		cursorPosY := int32(buffer.Cursor.Line) * int32(cursorSize.Y)
-		rl.DrawRectangleLines(cursorPosX, cursorPosY, int32(cursorSize.X), int32(cursorSize.Y), rl.Yellow)
+		editor.RenderBufferInWindow(&editor.Buffers[0], &editor.Windows[0])
+		
 		rl.EndDrawing()
 	}
 
+}
+
+
+var charSizeCache = map[byte]rl.Vector2{} //TODO: if font size or font changes this is fucked
+func measureTextSize(font rl.Font, s byte, size float32, spacing float32) rl.Vector2 {
+	if charSize, exists := charSizeCache[s]; exists {
+		return charSize
+	}
+	charSize := rl.MeasureTextEx(font, string(s), size, spacing)
+	charSizeCache[s] = charSize
+	return charSize
 }
