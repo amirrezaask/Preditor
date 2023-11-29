a7701ea0c24da5bdadfc562a49fa3a5e4af4510e
amirrezaask <raskarpour@gmail.com>
2023-11-29 19:20:25 +0330

    WIP: undo stack

---
+++ stack.go
@@ -0,0 +1,30 @@
+package main
+
+import "errors"
+
+type Stack[T any] struct {
+	data []T
+	size int
+}
+
+func NewStack[T any](size int) Stack[T] {
+	return Stack[T]{data: make([]T, size), size: size}
+}
+
+var (
+	EmptyStack = errors.New("empty stack")
+)
+
+func (s *Stack[T]) Pop() (T, error) {
+	if len(s.data) == 0 {
+		return *new(T), EmptyStack
+	}
+	return s.data[len(s.data)-1], nil
+}
+
+func (s *Stack[T]) Push(e T) {
+	s.data = append(s.data, e)
+	if len(s.data) > s.size {
+		s.data = []T{e}
+	}
+}
