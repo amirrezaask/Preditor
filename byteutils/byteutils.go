7930dd3d0601d5d2e229c8c88fcabf789a1dd173
a.askarpour <a.askarpour@corp.mabnadp.com>
2023-12-06 17:18:40 +0330

    Simplify word jumping

---
+++ byteutils/byteutils.go
@@ -0,0 +1,25 @@
+package byteutils
+
+import (
+	"bytes"
+)
+
+func SeekNextWhitespace(bs []byte, i int) int {
+	idx := bytes.IndexAny(bs[i:], " \n\r")
+	return i + idx
+}
+
+func SeekPreviousWhitespace(bs []byte, i int) int {
+	idx := bytes.LastIndexAny(bs[:i], " \n\r")
+	return idx
+}
+
+func PreviousWordInBuffer(bs []byte, idx int) int {
+	lastWhitespaceIndex := SeekPreviousWhitespace(bs, idx)
+	return lastWhitespaceIndex - 1
+}
+
+func NextWordInBuffer(bs []byte, idx int) int {
+	firstWhitespaceIndex := SeekNextWhitespace(bs, idx)
+	return firstWhitespaceIndex + 1
+}
