1a2803a3bf5f1d58df155746cd7e51dc13522bb7
amirrezaask <raskarpour@gmail.com>
2023-12-24 05:34:07 -0800

    Add my classic theme to default themes

--- lexer.go
+++ lexer.go
@@ -3,16 +3,36 @@ package preditor
 // Token is the basic data type of lexer
 // buffer[Start:End)
 type Token struct {
-	Data  []byte
 	Start int
 	End   int
+	Type  int
 }
 
-// Lexer produces tokens.
+// Lexer produces stream tokens.
 // We are already doing a single sweep on the buffer to split it
-// into visual lines that user see
-// we can do better by paying the same price but we get a language aware
+// into visual lines that user see, we can do better by paying the
+// same price but we get a language aware
 // tokens instead of visual lines and we can operate on tokens.
+/*
+	var lexer Lexer
+	var visualLines []Token
+	for {
+		token := lexer.Next()
+		if token.Len() + currentLine.Len() > maxChars {
+			append(currentLine)
+			make_new_visualLine()
+			currentLine.AddToken(token)
+		}
+	}
+	======
+
+	for line in visualLines.visiblePart() {
+		for token in line {
+			token.Draw(cfg) //handles color based on syntax
+		}
+	}
+	=======
+*/
 type Lexer interface {
-	Tokens(bs []byte) []Token
+	Next() Token
 }
