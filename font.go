6ae7fd51f16c4be657ef0dfb9ff1cf1ddc7c97f9
amirrezaask <raskarpour@gmail.com>
2023-12-24 05:33:54 -0800

    some renames, add missing double quotes

--- font.go
+++ font.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/flopp/go-findfont"
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
@@ -16,6 +17,33 @@ func measureTextSize(font rl.Font, s byte, size float32, spacing float32) rl.Vec
 
 
 var (
+	fontPath string
 	font     rl.Font
 	fontSize float32
 )
+
+func loadFont(name string, size float32) error {
+	var err error
+	fontPath, err = findfont.Find(name + ".ttf")
+	if err != nil {
+		return err
+	}
+
+	fontSize = size
+	font = rl.LoadFontEx(fontPath, int32(fontSize), nil)
+
+	return nil
+}
+
+func increaseFontSize(n int) {
+	fontSize += float32(n)
+	font = rl.LoadFontEx(fontPath, int32(fontSize), nil)
+	charSizeCache = map[byte]rl.Vector2{}
+}
+
+func decreaseFontSize(n int) {
+	fontSize -= float32(n)
+	font = rl.LoadFontEx(fontPath, int32(fontSize), nil)
+	charSizeCache = map[byte]rl.Vector2{}
+
+}
